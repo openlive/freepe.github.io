7eb54365f7d697ac537d67ff8503c768e3dbe69d
Bogdan Nekrasenko <Bogdan Nekrasenko>
2016-07-09 15:58:00 +0300

    build

--- src/js/partials/dashboard.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.list').on('click', function(){
-    $('.list-block').toggleClass('disp-none');
-  });
-  $('.view').on('click', function(){
-    $('.view-block').toggleClass('disp-none');
-  });
-});
--- src/js/partials/hide-body.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-$(document).ready(function(){
-  $('.hidden-block__header').on('click', function(){
-    $(this).toggleClass('show-body').parents('.hidden-block').find('.hidden-block__body').toggleClass('body-animate-hidden');
-  });
-});
